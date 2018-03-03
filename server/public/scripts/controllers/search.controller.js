257799606614342e809a985f402de81493cdd601
Jacob Albright <jacob.albright23@gmail.com>
2018-03-02 21:28:26 -0600

    Can display returned data on DOM. I'm going to bring in angular material now and try to make that work...will research angular material cards

--- server/public/scripts/controllers/search.controller.js
+++ server/public/scripts/controllers/search.controller.js
@@ -1,50 +1,51 @@
-app.controller('SearchController', ['SWAPIService as swapi', searchCtl]);
-
-function searchCtl (swapi) {
+app.controller('SearchController', ['SWAPIService as swapi', function(swapi) {
     const self = this;
     // list of searchable resources and their attributes
-    self.resources = {list: ['films', 'people', 'planets', 'species', 'starships', 'vehicles']}; 
+    self.resources = { list: ['films', 'people', 'planets', 'species', 'starships', 'vehicles'] };
     // pass this into Append this to request urls to get response translated into Wookiee
-    self.formatWookie = '?format=wookiee'; 
+    self.formatWookie = '?format=wookiee';
 
     // format wookiee checkbox (true of false)
     self.wookieeCheckBox = {
         value1: false,
     };
-    
-    
-    
 
     // Dynamic search bar placeholder
     self.searchBarPlaceholder = 'search...';
     self.searchField = (whichResource) => {
         console.log('in searchField', whichResource);
-        if (whichResource === 'films'){
-            console.log('whichResource is "films"'); 
+        if (whichResource === 'films') {
+            console.log('whichResource is "films"');
             self.searchBarPlaceholder = 'search titles';
         } else if (whichResource === 'people') {
             self.searchBarPlaceholder = 'search names';
-        } else if (whichResource === 'planets'){
+        } else if (whichResource === 'planets') {
             self.searchBarPlaceholder = 'search planet name';
-        } else if (whichResource === 'species'){
+        } else if (whichResource === 'species') {
             self.searchBarPlaceholder = 'search species name';
-        } else if (whichResource === 'starships'){
-            self.searchBarPlaceholder =  'search name or model';
-        } else if (whichResource === 'vehicles'){
+        } else if (whichResource === 'starships') {
+            self.searchBarPlaceholder = 'search name or model';
+        } else if (whichResource === 'vehicles') {
             self.searchBarPlaceholder = 'search name or model'
         } else {
-            self.searchBarPlaceholder =  'search...';
+            self.searchBarPlaceholder = 'search...';
         }
     }
 
     // swapi returned data objects
-    self.returnedData = swapi.returnedData
+    self.returned = swapi.returned
+
 
     // swapi $http references
     self.searchSWAPI = swapi.searchSWAPI;
     self.getAll = swapi.getAll;
 
 
+
+
     // swapi $http calls
 
-}
+}]);
+
+
+
--- server/public/scripts/services/swapi.service.js
+++ server/public/scripts/services/swapi.service.js
@@ -4,10 +4,10 @@ function swapiSvc ($http) {
     const self = this;
     const baseUrl = 'https://swapi.co/api/';
 
-    self.returnedData = {}; 
+    self.returned = {data: []}; 
 
     self.searchSWAPI = (whichResource, searchQuery, wookiee) => {
-        // console.log('whichResource', whichResource, 'searchQuery', searchQuery, 'wookiee', wookiee);
+        console.log('whichResource', whichResource, 'searchQuery', searchQuery, 'wookiee', wookiee);
         let swapiQuery;
         if (wookiee) {
             swapiQuery = baseUrl + whichResource + '/?search=' + searchQuery + '/?format=wookiee';
@@ -18,9 +18,9 @@ function swapiSvc ($http) {
             method: 'GET',
             url: swapiQuery
         }).then( (response) => {
-            self.returnedData = {};
-            self.returnedData = response.data;
-            console.log(self.returnedData);
+            self.returned.data = {};
+            self.returned.data = response.data;
+            console.log(self.returned.data);
         }).catch( (error) => {
             console.error('Error in self.searchSWAPI');
         });
@@ -32,14 +32,11 @@ function swapiSvc ($http) {
             method: 'GET',
             url: swapiQuery
         }).then((response) => {
-            self.returnedData = {};
-            self.returnedData = response.data;
-            console.log(self.returnedData);
+            self.returned.data = {};
+            self.returned.data = response.data;
         }).catch((error) => {
             console.error('Error in self.searchSWAPI');
         });
     }
-
-
 
 }
